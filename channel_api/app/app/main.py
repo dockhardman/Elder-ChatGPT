7fbf8574cd6dcb94bce01dd91ff44843dffaaa9a
Allen Chou <f1470891079@gmail.com>
2023-03-10 21:49:43 +0800

    Databases connect and create tables at ASGI event startup.

--- channel_api/app/app/main.py
+++ channel_api/app/app/main.py
@@ -1,9 +1,13 @@
 import logging
+from typing import TYPE_CHECKING
 
 import fastapi
 from fastapi.responses import PlainTextResponse
 
 from app.config import logger, settings, uvicorn_logger
+
+if TYPE_CHECKING:
+    import databases
 
 
 logger = logging.getLogger(settings.app_logger_name)
@@ -14,13 +18,33 @@ def create_app():
         title=settings.app_name,
     )
 
+    # Databases
+    from app.db.tracker_store import create_message_database, message_database
+
+    app.state.message_database = message_database
+
+    # ASGI Events
+    @app.on_event("startup")
+    async def startup() -> None:
+        message_database_: "databases.Database" = app.state.message_database
+        if not message_database_.is_connected:
+            await message_database_.connect()
+        create_message_database()
+
+    @app.on_event("shutdown")
+    async def shutdown() -> None:
+        message_database_: "databases.Database" = app.state.message_database
+        if message_database_.is_connected:
+            await message_database_.disconnect()
+
+    # Root Route
     @app.get("/")
     async def root():
         logger.debug("OK")
         uvicorn_logger.debug("OK")
         return PlainTextResponse("OK")
 
-    # Router
+    # Routers
     from app.api.router import router
 
     app.include_router(router, prefix=f"/channel")
